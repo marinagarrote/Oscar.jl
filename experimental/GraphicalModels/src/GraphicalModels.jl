b9a631c5719c9f99741d72b17bb62beac1f6466b
antonydellavecchia <antonydellavecchia@gmail.com>
2024-04-17 17:47:04 +0200

    adding minimal serialization

--- experimental/GraphicalModels/src/GraphicalModels.jl
+++ experimental/GraphicalModels/src/GraphicalModels.jl
@@ -1,3 +1,4 @@
 include("PhylogeneticModels.jl")
 include("PhylogeneticAuxiliary.jl")
 include("PhylogeneticParametrisation.jl")
+include("Serialization/tree_models.jl")
--- experimental/GraphicalModels/src/Serialization/tree_models.jl
+++ experimental/GraphicalModels/src/Serialization/tree_models.jl
@@ -1,7 +1,9 @@
-@register_serialization_type Oscar.PhylogeneticModel
+@register_serialization_type PhylogeneticModel
 
-function save_object(s::SerializerState, m::Oscar.PhylogeneticModel)
+function save_object(s::SerializerState, m::PhylogeneticModel)
   save_data_dict(s) do
     save_typed_object(s, graph(m), :graph)
+    save_typed_object(s, number_states(m), :n_states)
   end
 end
+
